0e63abd2ee884b0ba3d7add97178f9f73fb9c5d0
Gabriel Mariano Kunen <gabriel.marianok@gmail.com>
2025-10-22 21:19:25 -0300

    adicionado redis

--- Program.cs
+++ Program.cs
@@ -1,20 +1,57 @@
+using System.Net;
 using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
 using WeatherApi.Dto;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var redis = ConnectionMultiplexer.Connect(builder.Configuration["Redis:ConnectionString"]!);
+builder.Services.AddSingleton<IConnectionMultiplexer>(redis);
+
 var app = builder.Build();
 
-app.MapGet("/teste", async () =>
+app.MapGet("/weather", async (
+    [FromQuery] string? localizacao, 
+    [FromQuery] string? dataInicial, 
+    [FromQuery] string? dataFinal,
+    IConnectionMultiplexer redisConnection) =>
 {
-    var httpClient = new HttpClient();
-    var url = $"{builder.Configuration["WeatherApi:Url"]}";
-    url += "VisualCrossingWebServices/rest/services/timeline/Criciuma,BR/2025-10-17/2025-10-17?";
-    url += $"key={builder.Configuration["WeatherApi:ApiKey"]}";
-    var retornoApi = await httpClient.GetAsync(url);
-    retornoApi.EnsureSuccessStatusCode();
-    var conteudo = await retornoApi.Content.ReadAsStringAsync();
-    var retornoApiDto = JsonSerializer.Deserialize<WeatherApiRetorno>(conteudo);
-    return Results.Ok(retornoApiDto);
+    try
+    {
+        var db = redisConnection.GetDatabase();
+        var jsonRedis = await db.StringGetAsync(localizacao);
+        if (jsonRedis.HasValue)
+            return Results.Ok(JsonSerializer.Deserialize<WeatherApiRetorno>(jsonRedis!));
+    
+        var httpClient = new HttpClient();
+        var url = $"{builder.Configuration["WeatherApi:Url"]}";
+        url += $"VisualCrossingWebServices/rest/services/timeline/{localizacao}/{dataInicial}/{dataFinal}?";
+        url += $"key={builder.Configuration["WeatherApi:ApiKey"]}";
+        var retornoApi = await httpClient.GetAsync(url);
+        retornoApi.EnsureSuccessStatusCode();
+        var conteudo = await retornoApi.Content.ReadAsStringAsync();
+        var retornoApiDto = JsonSerializer.Deserialize<WeatherApiRetorno>(conteudo);
+    
+        var json = JsonSerializer.Serialize(retornoApiDto);
+        await db.StringSetAsync(localizacao, json,  TimeSpan.FromMinutes(5));
+    
+        return Results.Ok(retornoApiDto);
+    }
+    catch (HttpRequestException httpException)
+    {
+        if (httpException.StatusCode == HttpStatusCode.Unauthorized)
+            return Results.InternalServerError("Ocorreu um erro ao se comunicar com a WeatherAPI. Tente novamente mais tarde.");
+        
+        if (httpException.StatusCode == HttpStatusCode.BadRequest)
+            return Results.BadRequest("A localização informada não existe.");
+
+        return Results.InternalServerError("Ocorreu um erro ao se comunicar com a WeatherAPI. Tente novamente mais tarde.");
+    }
+    catch (Exception)
+    {
+        return Results.InternalServerError("Ocorreu um erro interno. Tente novamente mais tarde.");
+    }
 });
 
 app.Run();
